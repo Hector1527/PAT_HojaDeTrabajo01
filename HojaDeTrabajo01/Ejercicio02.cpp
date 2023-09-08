e4efc2a5b7f06c243861e0e577f9aa55e79bfe75
Hector1527 <hectorperezgonzalez09@gmail.com>
2023-09-07 19:34:18 -0600

    Cambios en los dos ejercicios

--- HojaDeTrabajo01/Ejercicio02.cpp
+++ HojaDeTrabajo01/Ejercicio02.cpp
@@ -1,6 +1,14 @@
 #include "Ejercicio02.h"
+#include <algorithm>
+#include <string>
 
 bool Ejercicio02::isPalindrome(string& s)
 {
-    return false;
+        s.erase(remove_if(s.begin(), s.end(), [](char c) {
+            return !isalpha(c);
+            }), s.end());
+
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+        return s == string(s.rbegin(), s.rend());
 }
--- HojaDeTrabajo01/HojaDeTrabajo01.cpp
+++ HojaDeTrabajo01/HojaDeTrabajo01.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hector Pérez - 1304223 \n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
